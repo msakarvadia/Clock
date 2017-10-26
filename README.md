a911714833fcffbf64046b932614f8a5ab9dabe8
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2017-10-26 19:10:26 -0400

    V1

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a customizable clock application, which will help you stay on time. It is written in JavaScript, HTML, and CSS. 
+
+clone this repository, point your browser to your copy of this repository, and open the HTML file. If you right click, and click inspect, you will be able to see the HTML code, and the links to the CSS and JS files will be easily accessible.
+
+This is my 11th grade final project in Dr. Morrison's Web Development class at NCSSM.
---
+++ sakarvadia19mFPP1.js
@@ -0,0 +1,103 @@
+window.addEventListener("load", init);
+
+function init()
+{
+  var c=document.getElementById("canvas");
+  var ctx=c.getContext("2d");
+
+  var centerX = canvas.width / 2;
+  var centerY = canvas.height / 2;
+  //some of this code is borrowed from http://findnerd.com/list/view/A-Concentric-Circle-Clock-Using-JavaScript-and-Canvas/22842/
+var now = new Date();
+//var time = now.toLocaleTimeString();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds(); //define seconds inside draw
+
+//  var seconddeg = seconds*6;
+  //var minutedeg = minutes*6;
+  //var hourdeg = hours%12*30;
+
+window.setInterval(drawHands, 20);
+
+
+function drawHands()
+{
+//when calculating the end point of the hands, the coordinates get a little tricky:
+//we have to add the cos value to the Y coordinate and sin value to the x coordinate, because the clock is not oriented like the unit circle
+//the y value must be negative becuase the clock is moving clockwise, as opposed to the counter clockwise unit circle
+now =new Date();
+  ctx.beginPath();
+  ctx.arc(centerX,centerY,200,0,2*Math.PI);
+  ctx.fillStyle = 'lightsalmon';
+  ctx.fill();
+  ctx.closePath();
+
+  ctx.beginPath();
+  ctx.arc(centerX,centerY,20,0,2*Math.PI);
+  ctx.fillStyle = 'gray';
+  ctx.fill();
+  ctx.closePath();
+//draws second hand
+
+  ctx.beginPath();
+
+  ctx.moveTo(centerX, centerY);
+  ctx.lineTo(centerX+100*Math.sin((now.getSeconds()*6)*Math.PI /180),
+  centerY+100*-Math.cos((now.getSeconds()*6)*Math.PI /180));
+  ctx.stroke();
+//draws minutes hand
+
+  ctx.beginPath();
+  ctx.moveTo(centerX, centerY);
+  ctx.lineTo(centerX+150*Math.sin((now.getMinutes()*6)*Math.PI /180),
+  centerY+150*-Math.cos((now.getMinutes()*6)*Math.PI /180));
+  ctx.stroke();
+//draws hours hand
+
+  ctx.beginPath();
+  ctx.moveTo(centerX, centerY);
+//  ctx.lineTo((centerX+200*Math.sin(now.getHours()*30*Math.PI /180+now.getMinutes()*.5*Math.PI /180)),
+  //(centerY+200*-Math.cos(now.getHours()*30*Math.PI/180 +now.getMinutes()*.5*Math.PI /180)));
+  ctx.lineTo((centerX+200*Math.sin( (now.getHours()*30*Math.PI/180) +now.getMinutes()*.5*Math.PI /180)),
+  (centerY+200*-Math.cos( (now.getHours()*30*Math.PI/180) +now.getMinutes()*.5*Math.PI /180)));
+
+  ctx.stroke();
+}
+
+document.getElementById("alarmButton").addEventListener("click", alarm())
+
+
+function alarm()
+{
+var now = new Date();
+var alarmPrompt = prompt("Enter the time you want to sound the alarm.", "HH:MM:SS")
+  if (alarmPrompt!= null)
+  {
+    //check that the input is correct
+        //check type of input, lenght of input
+    var alarmHour = alarmPrompt.slice(0,2);
+    var alarmmin = alarmPrompt.slice(3,5);
+    var alarmsec = alarmPrompt.slice(6,8);
+
+    //print: "ur alarm will cound in __ hours, __ minutes __ seconds"
+
+    if (alarmHour==now.hours() && alarmmin==now.minutes() && alarmsec==now.seconds());
+      {
+        //sound the alarm
+      }
+
+    //figure out time for alarm
+
+
+  }
+
+}
+
+function timer()
+{
+
+}
+
+
+}
