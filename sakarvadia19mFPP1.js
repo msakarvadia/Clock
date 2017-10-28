08fcc993c8a9519d6fc88d96ab00fdcc0fdde6c4
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2017-10-28 15:44:58 -0400

    balls appear every second, minute, and hour, but seconds don't get ereased and restarted every minute, minute balls don't get eareased and restared every hour, and hour balls don't get earased and restared every 12 hours.[C

--- sakarvadia19mFPP1.js
+++ sakarvadia19mFPP1.js
@@ -17,62 +17,82 @@ function init()
   var hours = now.getHours();
   var minutes = now.getMinutes();
   var seconds = now.getSeconds(); //define seconds inside draw
+  //end of borrowed code
+
+  window.setInterval(drawHands, 20);
 
 
+  function drawHands()
+  {
+      //when calculating the end point of the hands, the coordinates get a little tricky:
+      //we have to add the cos value to the Y coordinate and sin value to the x coordinate, because the clock is not oriented like the unit circle
+      //the y value must be negative becuase the clock is moving clockwise, as opposed to the counter clockwise unit circle
+      now =new Date();
+        ctx.beginPath();
+        ctx.arc(centerX,centerY,200,0,2*Math.PI);
+        ctx.fillStyle = 'lightsalmon';
+        ctx.fill();
+        ctx.closePath();
 
-//  var seconddeg = seconds*6;
-  //var minutedeg = minutes*6;
-  //var hourdeg = hours%12*30;
+        ctx.beginPath();
+        ctx.arc(centerX,centerY,20,0,2*Math.PI);
+        ctx.fillStyle = 'gray';
+        ctx.fill();
+        ctx.closePath();
+      //draws second hand
 
-window.setInterval(drawHands, 20);
+        ctx.beginPath();
 
+        ctx.moveTo(centerX, centerY);
+        ctx.lineTo(centerX+180*Math.sin((now.getSeconds()*6)*Math.PI /180),
+        centerY+180*-Math.cos((now.getSeconds()*6)*Math.PI /180));
+        ctx.stroke();
 
-function drawHands()
-{
-//when calculating the end point of the hands, the coordinates get a little tricky:
-//we have to add the cos value to the Y coordinate and sin value to the x coordinate, because the clock is not oriented like the unit circle
-//the y value must be negative becuase the clock is moving clockwise, as opposed to the counter clockwise unit circle
-now =new Date();
-  ctx.beginPath();
-  ctx.arc(centerX,centerY,200,0,2*Math.PI);
-  ctx.fillStyle = 'lightsalmon';
-  ctx.fill();
-  ctx.closePath();
+//draw a ball when one second passes
 
-  ctx.beginPath();
-  ctx.arc(centerX,centerY,20,0,2*Math.PI);
-  ctx.fillStyle = 'gray';
-  ctx.fill();
-  ctx.closePath();
-//draws second hand
+        ctx.beginPath();
+        ctx.arc((centerX+230*Math.sin((now.getSeconds()*6)*Math.PI /180)),
+        (centerY+230*-Math.cos((now.getSeconds()*6)*Math.PI /180)), 3,0,2*Math.PI);
+        ctx.fillStyle="gray";
+        ctx.fill();
+        ctx.closePath();
 
-  ctx.beginPath();
+//  if(now.getSeconds()==0)
+//  {
+//        ctx.clearRect(0,0,canvas.width, canvas.height);
+//  }
+      //draws minutes hand
+        ctx.beginPath();
+        ctx.moveTo(centerX, centerY);
+        ctx.lineTo(centerX+150*Math.sin((now.getMinutes()*6)*Math.PI /180),
+        centerY+150*-Math.cos((now.getMinutes()*6)*Math.PI /180));
+        ctx.stroke();
 
-  ctx.moveTo(centerX, centerY);
-  ctx.lineTo(centerX+180*Math.sin((now.getSeconds()*6)*Math.PI /180),
-  centerY+180*-Math.cos((now.getSeconds()*6)*Math.PI /180));
-  ctx.stroke();
-//draws minutes hand
+        //draw a ball when one minute passes
+        ctx.beginPath();
+        ctx.arc(centerX+250*Math.sin((now.getMinutes()*6)*Math.PI /180),
+        centerY+250*-Math.cos((now.getMinutes()*6)*Math.PI /180), 5,0,2*Math.PI);
+        ctx.fillStyle="gray";
+        ctx.fill();
+        ctx.closePath();
 
-  ctx.beginPath();
-  ctx.moveTo(centerX, centerY);
-  ctx.lineTo(centerX+150*Math.sin((now.getMinutes()*6)*Math.PI /180),
-  centerY+150*-Math.cos((now.getMinutes()*6)*Math.PI /180));
-  ctx.stroke();
-//draws hours hand
+      //draws hours hand
+        ctx.beginPath();
+        ctx.moveTo(centerX, centerY);
+        ctx.lineTo((centerX+100*Math.sin( (now.getHours()*30*Math.PI/180) +now.getMinutes()*.5*Math.PI /180)),
+        (centerY+100*-Math.cos( (now.getHours()*30*Math.PI/180) +now.getMinutes()*.5*Math.PI /180)));
+        ctx.stroke();
 
-  ctx.beginPath();
-  ctx.moveTo(centerX, centerY);
-//  ctx.lineTo((centerX+200*Math.sin(now.getHours()*30*Math.PI /180+now.getMinutes()*.5*Math.PI /180)),
-  //(centerY+200*-Math.cos(now.getHours()*30*Math.PI/180 +now.getMinutes()*.5*Math.PI /180)));
-  ctx.lineTo((centerX+100*Math.sin( (now.getHours()*30*Math.PI/180) +now.getMinutes()*.5*Math.PI /180)),
-  (centerY+100*-Math.cos( (now.getHours()*30*Math.PI/180) +now.getMinutes()*.5*Math.PI /180)));
+      //draw a ball when one hour passes
+      ctx.beginPath();
+      ctx.arc((centerX+300*Math.sin( (now.getHours()*30*Math.PI/180))),
+      (centerY+300*-Math.cos( (now.getHours()*30*Math.PI/180))), 7,0,2*Math.PI);
+      ctx.fillStyle="gray";
+      ctx.fill();
+      ctx.closePath();
 
-  ctx.stroke();
-}
-
-  document.getElementById("alarmButton").addEventListener("click", setAlarm);
-
+  }
+    document.getElementById("alarmButton").addEventListener("click", setAlarm);
 }
 
 
@@ -82,7 +102,7 @@ function setAlarm()
 {
 	var now = new Date();
 	    alarmPrompt = prompt("Enter the time you want to sound the alarm. Enter in Military time.", "HH:MM:SS");
-			if (alarmPrompt.search(/[0-2][0-3]:[0-5][0-9]:[0-5][0-9]/) == 0)
+			if (alarmPrompt.search(/([2][0-3]|[0-1][0-9]):[0-5][0-9]:[0-5][0-9]/) == 0)
 			{
         document.getElementById("Aconfirm").innerHTML = "Your Alarm is set to " + alarmPrompt;
         var now = new Date();
@@ -108,6 +128,7 @@ function soundAlarm()
 {
     if (!alarmFlag)
     {
+      //do this later: document.getElementById("alarmButton").innerHTML = "STOP ALARM";
       document.getElementById('stopAlarmButton').addEventListener("click", stopSound);
       return;
     }
